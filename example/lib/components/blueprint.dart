9ce611afc8eb4a550d1b097502a2cc096eada8d0
Yauhen Landarski <landarskiy@gmail.com>
2023-05-29 14:35:10 +0200

    Update sample

--- example/lib/components/blueprint.dart
+++ example/lib/components/blueprint.dart
@@ -8,13 +8,17 @@ class BlueprintBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(1.0),
+      padding: const EdgeInsets.all(2.0),
       child: SizedBox.expand(
         child: DecoratedBox(
           decoration: BoxDecoration(
             color: andreaBlue,
             shape: BoxShape.rectangle,
-            border: Border.all(color: Colors.white, style: BorderStyle.solid),
+            border: Border.all(
+              color: white,
+              style: BorderStyle.solid,
+              width: 1,
+            ),
           ),
         ),
       ),
@@ -29,17 +33,30 @@ class ContentBlueprintBox extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: [
-        const BlueprintBox(),
-        Center(
-          child: Text(
-            text,
-            textAlign: TextAlign.center,
-            style: Theme.of(context).textTheme.bodyMedium,
+    final textStyle =
+        Theme.of(context).textTheme.bodyMedium?.copyWith(color: andreaBlue);
+    return Padding(
+      padding: const EdgeInsets.all(2.0),
+      child: SizedBox.expand(
+        child: DecoratedBox(
+          decoration: BoxDecoration(
+            color: white,
+            shape: BoxShape.rectangle,
+            border: Border.all(
+              color: heatWave,
+              style: BorderStyle.solid,
+              width: 3,
+            ),
+          ),
+          child: Center(
+            child: Text(
+              text,
+              textAlign: TextAlign.center,
+              style: textStyle,
+            ),
           ),
         ),
-      ],
+      ),
     );
   }
 }
